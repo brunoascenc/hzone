140060b7d73f9c4b5a2652d7be9902e048616120
brunoascenc <brunoascenc1@outlook.com>
2020-10-07 21:34:00 -0300

    netlify deploy

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useContext } from "react";
+import Modal from "react-modal";
+import { DataContext } from "../data/DataProvider";
+import { VscClose } from "react-icons/vsc";
+import "../App.css";
+import StripeCheckout from "react-stripe-checkout";
+import { Link } from "react-router-dom";
+
+const Checkout = ({ total, product }) => {
+  const value = useContext(DataContext);
+  const [cart] = value.cart;
+  const [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const handleCheckout = () => {
+    openModal();
+    window.localStorage.clear();
+  };
+
+  //Reset cart
+  function closeModal() {
+    setIsOpen(false);
+    cart.length = 0;
+  }
+
+  return (
+    <div>
+      <Modal
+        closeTimeoutMS={200}
+        className="modal"
+        isOpen={modalIsOpen}
+        ariaHideApp={false}
+        // onRequestClose={closeModal}
+      >
+        <div className="modal-content">
+          <button onClick={closeModal}>
+            <Link to="/">
+              <VscClose />
+            </Link>
+          </button>
+          <h1>Compra realizada</h1>
+          <p>Seu pedido está a caminho!</p>
+        </div>
+      </Modal>
+      <StripeCheckout
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+        token={handleCheckout}
+        name="Pagamento"
+        amount={total * 100}
+        shippingAddress
+        billingAddress
+        alipay
+      >
+        <button>Finalizar Compra</button>
+      </StripeCheckout>
+    </div>
+  );
+};
+
+export default Checkout;
