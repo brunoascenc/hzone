2c92bc36675f38f78877efed29fcc5907dd9521e
brunoascenc <brunoascenc1@outlook.com>
2021-06-14 17:29:16 -0300

    slider banners

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation, Autoplay, Pagination } from "swiper";
-import { Link } from "react-router-dom";
-import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Autoplay, Pagination } from 'swiper';
+import { Link } from 'react-router-dom';
+import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri';
 // Import Swiper styles
-import "swiper/swiper.scss";
-import "swiper/components/navigation/navigation.scss";
-import "swiper/components/pagination/pagination.scss";
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
 SwiperCore.use([Navigation, Autoplay, Pagination]);
 
 const Slider = () => {
@@ -15,7 +15,7 @@ const Slider = () => {
     <div className="swiper-wrapper">
       <Swiper
         slidesPerView={1}
-        navigation={{ nextEl: ".right-arrow", prevEl: ".left-arrow" }}
+        navigation={{ nextEl: '.right-arrow', prevEl: '.left-arrow' }}
         pagination={{
           clickable: true,
           dynamicBullets: true,
@@ -25,25 +25,25 @@ const Slider = () => {
         className="slider"
       >
         <SwiperSlide
-          className="slider-card"
+          className="slider-card galaxy"
           style={{
             background: `linear-gradient(rgba(31, 28, 28, 0.329),rgba(14, 14, 13, 0.89)), url(
-              "https://images.unsplash.com/photo-1551764046-eadb20826deb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80"
+              "https://gizchina.it/wp-content/uploads/2021/01/Samsung-galaxy-s21-0.jpg"
             )100% center no-repeat`,
           }}
         >
           <div className="container">
             <div>
-              <h2>Samsung Galaxy s20</h2>
+              <h2>Samsung Galaxy s21</h2>
               <Link to="/samsung">Compre agora</Link>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide
-          className="slider-card"
+          className="slider-card apple"
           style={{
             background: `linear-gradient(rgba(31, 28, 28, 0.329),rgba(14, 14, 13, 0.89)), url(
-              "https://images.unsplash.com/photo-1608022625050-82683640e5a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1503&q=80"
+              "https://i2.wp.com/ihelpbr.com/wp-content/uploads/2020/12/iPhone-12-e-12-Pro.png?resize=1920%2C1080&ssl=1"
             ) center no-repeat`,
           }}
         >
@@ -55,17 +55,17 @@ const Slider = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide
-          className="slider-card"
+          className="slider-card motorola"
           style={{
             background: `linear-gradient(rgba(31, 28, 28, 0.329),rgba(22, 22, 21, 0.527)), url(
-              "https://images.unsplash.com/photo-1590653956132-7124afce5a9d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
-            ) center no-repeat`,
+              "https://tecnoblog.net/wp-content/uploads/2020/03/motorola-edge-plus-evleaks.jpg"
+            ) top no-repeat`,
           }}
         >
           <div className="container">
             <div>
-              <h2>Asus aog</h2>
-              <Link to="/asus">Compre agora</Link>
+              <h2>Motorola Edge</h2>
+              <Link to="/motorola">Compre agora</Link>
             </div>
           </div>
         </SwiperSlide>
